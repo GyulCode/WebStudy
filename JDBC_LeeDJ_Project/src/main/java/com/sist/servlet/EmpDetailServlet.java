7daf9baf16a3c6c24a076e895e3ba14fdb58902d
GyulCode <ldg2057@naver.com>
2023-06-08 15:53:39 +0900

    0608_추가

--- JDBC_LeeDJ_Project/src/main/java/com/sist/servlet/EmpDetailServlet.java
+++ JDBC_LeeDJ_Project/src/main/java/com/sist/servlet/EmpDetailServlet.java
@@ -27,68 +27,61 @@ public class EmpDetailServlet extends HttpServlet {
 		//vo에 있는 데이터를 출력
 		PrintWriter out=response.getWriter();
 		
-		//HTML 출력 시작
-		  out.write("<!DOCTYPE html>");
+		 // HTML 출력 시작
+	     out.write("<!DOCTYPE html>");
 	      out.write("<html>");
 	      out.write("<head>");
 	      out.write("<meta charset=\"UTF-8\">");
 	      out.write("<title>Insert title here</title>");
 	      out.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">");
-	      out.write("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js\"></script>");
-	      out.write("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\"></script>");
+	      out.write("<style>");
+	      out.write(".container{margin-top:50px}");
+	      out.write(".row{margin:0px auto;width:600px}");
+	      out.write("h1{text-align:center}");
+	      out.write("</style>");
 	      out.write("</head>");
 	      out.write("<body>");
 	      out.write("<div class=container>");
-	      
 	      out.write("<h1>"+vo.getEname()+"사원의 개인정보</h1>");
-	      
 	      out.write("<div class=row>");
-	      out.write("<table>");
-	      
+	      out.write("<table class=table>");
 	      out.write("<tr>");
 	      out.write("<th class=\"text-center danger\" width=20%>사번</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getEmpno()+"</td>");
 	      out.write("<th class=\"text-center danger\" width=20%>이름</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getEname()+"</td>");
 	      out.write("</tr>");
-	      
 	      out.write("<tr>");
 	      out.write("<th class=\"text-center danger\" width=20%>직위</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getJob()+"</td>");
 	      out.write("<th class=\"text-center danger\" width=20%>입사일</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getDbday()+"</td>");
 	      out.write("</tr>");
-	      
 	      out.write("<tr>");
 	      out.write("<th class=\"text-center danger\" width=20%>급여</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getSal()+"</td>");
 	      out.write("<th class=\"text-center danger\" width=20%>성과급</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+(vo.getComm()==0?"":vo.getComm())+"</td>");
 	      out.write("</tr>");
-	      
 	      out.write("<tr>");
 	      out.write("<th class=\"text-center danger\" width=20%>사수번호</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+(vo.getMgr()==0?"":vo.getMgr())+"</td>");
 	      out.write("<th class=\"text-center danger\" width=20%>부서명</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getDvo().getDname()+"</td>");
 	      out.write("</tr>");
-	      
 	      out.write("<tr>");
 	      out.write("<th class=\"text-center danger\" width=20%>근무지</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getDvo().getLoc()+"</td>");
 	      out.write("<th class=\"text-center danger\" width=20%>호봉</th>");
 	      out.write("<td class=\"text-center\" width=30%>"+vo.getSvo().getGrade()+"</td>");
 	      out.write("</tr>");
-	      
 	      out.write("<tr>");
 	      out.write("<td colspan=4 class=text-right>");
 	      out.write("<a href=# class=\"btn btn-xs btn-danger\">수정</a>&nbsp;");
 	      out.write("<a href=# class=\"btn btn-xs btn-success\">삭제</a>&nbsp;");
-	      out.write("<a href=Mainservlet class=\"btn btn-xs btn-primary\">목록</a>");
-	      
-	      
+	      out.write("<a href=MainServlet class=\"btn btn-xs btn-primary\">목록</a>");
+	      out.write("</td>");
 	      out.write("</tr>");
-	      
 	      out.write("</table>");
 	      out.write("</div>");
 	      out.write("</div>");
